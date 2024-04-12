0209b98d79ea44e3cac21079987cb18314cdf771
lucascauanh <lucascauanh10@gmail.com>
2024-04-12 11:35:14 -0300

    criado projeto portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
+import {HttpClientModule} from '@angular/common/http';
+import { MovieApiServiceService } from './service/movie-api-service.service';
+
+import {ReactiveFormsModule} from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    MovieDetailsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [MovieApiServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
